d6e722101c9ac0d6da92eb4e1814e905a2bf3ecb
Ivan Plyushchenko <plyush1993@bk.ru>
2021-10-18 16:29:06 +0300

    Update 4. Correction.R

--- Scripts (R)/4. Correction.R
+++ Scripts (R)/4. Correction.R
@@ -886,7 +886,7 @@ QC.SM.GB <- function(int_data, order, class, qc_label, model = "xgboost", max.de
   if (model == "catboost") {
     library(catboost)
     catb_train <- pblapply(2:ncol(int_data_ro_qc), function(t) catboost.load_pool(data = as.matrix(as.numeric(ro_qc)), label = as.matrix(as.numeric(int_data_ro_qc[,t]))))
-    catb_test <- pblapply(2:ncol(int_data_ro), function(t) catboost.load_pool(data = as.matrix(as.numeric(ro)), label = as.matrix(as.numeric(f_d_ro[,t]))))
+    catb_test <- pblapply(2:ncol(int_data_ro), function(t) catboost.load_pool(data = as.matrix(as.numeric(ro)), label = as.matrix(as.numeric(int_data_ro[,t]))))
     
     fit_catb <- pblapply(1:length(catb_train), function(t) catboost.train(catb_train[[t]], params = params))
     predict_gb <- pblapply(1:length(fit_catb), function(t) catboost.predict(fit_catb[[t]], catb_test[[t]], verbose = F)) 
