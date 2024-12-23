146558f2a9ef078eda680abc4913419f648ed2a4
Ivan Plyushchenko <plyush1993@bk.ru>
2024-12-24 00:17:15 +0300

    Add files via upload

--- Auxiliary files (RData)/Supp Code for MetCorR.R
+++ Auxiliary files (RData)/Supp Code for MetCorR.R
@@ -6,7 +6,11 @@ if(!"pacman" %in% rownames(installed.packages())){
   install.packages("pacman")
 }
 cat("Checking required packages (auto-installing if missing)\n")
-pacman::p_load("data.table", "dplyr", "stringr", "ggplot2", "ggsci")
+pacman::p_load("data.table", "dplyr", "stringr", "ggplot2", "ggsci", "mgcv", "remotes")
+
+if(!require(ggmagnify)){
+  install_github("hughjonesd/ggmagnify")
+  library(ggmagnify)}
 
 dsr <-as.data.frame(fread(input = "https://github.com/plyush1993/OUKS/raw/refs/heads/main/Datasets%20(csv)/xcms%20after%20IPO%20MVI.csv", header=T)) # first column with all metadata
 rownames(dsr) <- dsr[,1]
@@ -89,9 +93,17 @@ fwrite(ds_gam2, "xcms after IPO MVI QC-GAM2.csv", row.names = T)
 #........................models overview.........................
 
 # with by s term
-model <- gam(log2(tic_int_data_qc) ~ s(ro_qc, by = batch_qc))
-vis.gam(model, view = c("ro_qc", "batch_qc"),
+model1r <- gam(log2(tic_int_data_qc) ~ s(ro_qc, by = batch_qc))
+vis.gam(model1r, view = c("ro_qc", "batch_qc"),
         theta = 50, n.grid = 50, lwd = 0.4)
+predict_gam1r = predict(model1r, newdata=data.frame(ro_qc = ro, batch_qc = batch))
+
+# with 2 s terms
+model2r <- gam(log2(tic_int_data_qc) ~ s(ro_qc)+s(batch_qc))
+predict_gam1 = predict(model2r, newdata=data.frame(ro_qc = ro, batch_qc = batch))
+vis.gam(model2r, view = c("ro_qc", "batch_qc"),
+        theta = 50, n.grid = 50, lwd = 0.4)
+predict_gam2r = predict(model2r, newdata=data.frame(ro_qc = ro, batch_qc = batch))
 
 # with simple s term
 model1 <- gam(log2(tic_int_data_qc) ~ s(ro_qc))
@@ -113,18 +125,39 @@ legend('topright', legend=c('s(ro_qc)', 's(ro_qc, batch_qc)'),
 #.............................ggplot.............................
 init_data <- as.data.frame(cbind(Abundance = log2(tic_int_data_qc), Order = ro_qc, Batch = batch_qc))
 data <- as.data.frame(cbind(Order = ro, 
-                            Model_1 = predict_gam1,
-                            Model_2 = predict_gam2))
+                            's(order)' = predict_gam1,
+                            's(order, batch)' = predict_gam2,
+                            's(order, by=batch)' = predict_gam1r,
+                            's(order)+s(batch)' = predict_gam2r))
 data <- melt(data, id = "Order")
 
 ggplot(init_data, aes(y = Abundance, x = Order)) + 
-  geom_point(size=3.5, color="black", aes(fill= as.factor(Batch)), alpha=0.5, shape=21, stroke=0.5) + scale_fill_viridis_d(guide = 'none')+
-  theme_classic(base_size = 15) + theme(legend.position = c(0.7, 0.9)) + labs(fill = "Batch")+labs(colour = "Model:") +
-  geom_line(data = data, aes(x = Order, y = value, colour = variable), alpha = 1, linewidth = 1.5)+scale_color_aaas() 
+  geom_rect(aes(xmin=95, xmax=117, ymin=29, ymax= 29.4), color = "black", fill="grey89") +
+  geom_rect(aes(xmin=197, xmax=225, ymin=28.5, ymax= 29), color = "black", fill="grey89") +
+  geom_point(size=3.5, color="black", fill= "white", shape=21, stroke=1) +
+  theme_classic(base_size = 15) + theme(legend.position = c(0.8, 0.85)) + labs(fill = "Batch")+labs(colour = "Model:") +
+  geom_line(data = data, aes(x = Order, y = value, colour = variable), alpha = 1, 
+            linewidth = 1.2)+scale_color_d3() 
 
 ggplot(init_data, aes(y = Abundance, x = Order)) + 
+  geom_point(size=3.5, color="black", fill= "grey90", shape=21, stroke=1) + 
+  theme_linedraw(base_size = 15) + theme(legend.position = "top", legend.title=element_blank(),
+                                   legend.key.height  = unit(0.02, 'cm'), legend.text = element_text(size=15)) + 
+  labs(fill = "Batch")+labs(colour = "Model:") +
+  geom_line(data = data, aes(x = Order, y = value, color = variable), alpha = 1, linewidth = 1.5)+scale_color_jco() + 
+    geom_magnify(from = c(xmin = 95, xmax = 117, ymin = 29, ymax = 29.4), 
+               to = c(60, 135, 28.4, 28.9),
+               proj.fill = alpha("yellow", 0.2)) +
+  geom_magnify(from = c(xmin = 197, xmax = 225, ymin = 28.5, ymax = 29), 
+               to = c(160, 250, 29.2, 29.7),
+               proj.fill = alpha("yellow", 0.2))+
+  guides(color = guide_legend(keyheight = unit(0.1, 'cm')))
+  
+ggplot(init_data, aes(y = Abundance, x = Order)) + 
+  geom_rect(aes(xmin=95, xmax=117, ymin=29, ymax= 29.4), color = "black", fill="grey89") +
+  geom_rect(aes(xmin=197, xmax=225, ymin=28.5, ymax= 29), color = "black", fill="grey89") +
   geom_point(size=3.5, color="black", aes(fill= as.factor(Batch)), alpha=0.5, shape=21, stroke=0.5) + scale_fill_viridis_d(guide = 'none')+
-  theme_classic(base_size = 15) + theme(legend.position = "none") + labs(fill = "Batch")+labs(colour = "Model:") +
+  theme_bw(base_size = 15) + theme(legend.position = "none") + labs(fill = "Batch")+labs(colour = "Model:") +
   geom_line(data = data, aes(x = Order, y = value, colour = variable), alpha = 1, linewidth = 1.5)+scale_color_aaas()+
   facet_grid(~variable)
 #................................................................
