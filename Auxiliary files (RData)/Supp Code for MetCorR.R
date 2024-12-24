6f270ab6f52ebbe544c858568e074abe18dccf4a
Ivan Plyushchenko <plyush1993@bk.ru>
2024-12-24 17:12:52 +0300

    Add files via upload

--- Auxiliary files (RData)/Supp Code for MetCorR.R
+++ Auxiliary files (RData)/Supp Code for MetCorR.R
@@ -91,7 +91,6 @@ fwrite(ds_gam2, "xcms after IPO MVI QC-GAM2.csv", row.names = T)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #........................models overview.........................
-
 # with by s term
 model1r <- gam(log2(tic_int_data_qc) ~ s(ro_qc, by = batch_qc))
 vis.gam(model1r, view = c("ro_qc", "batch_qc"),
@@ -162,4 +161,83 @@ ggplot(init_data, aes(y = Abundance, x = Order)) +
   theme_bw(base_size = 15) + theme(legend.position = "none") + labs(fill = "Batch")+labs(colour = "Model:") +
   geom_line(data = data, aes(x = Order, y = value, colour = variable), alpha = 1, linewidth = 1.5)+scale_color_aaas()+
   facet_grid(~variable)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##             particular feature as a model signal            ~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#................select feature by SD and mean int...............
+sd_v <- apply(ds[qc_id,], 2, sd, na.rm = T)
+mean_v <- apply(ds[qc_id,], 2, mean, na.rm = T)
+hist(mean_v)
+rsd_v <- sd_v/mean_v*100
+hist(rsd_v)
+#........................choose threshold........................
+sel_features_ind <- which(rsd_v > 80 & mean_v > 10e5)
+#.........................choose feature.........................
+f_int <- ds[qc_id,which.max(rsd_v[sel_features_ind])]
+plot(log2(f_int))
+
+#........................models overview.........................
+# with by s term
+model1r <- gam(log2(f_int) ~ s(ro_qc, by = batch_qc))
+vis.gam(model1r, view = c("ro_qc", "batch_qc"),
+        theta = 50, n.grid = 50, lwd = 0.4)
+predict_gam1r = predict(model1r, newdata=data.frame(ro_qc = ro, batch_qc = batch))
+
+# with 2 s terms
+model2r <- gam(log2(f_int) ~ s(ro_qc)+s(batch_qc))
+predict_gam1 = predict(model2r, newdata=data.frame(ro_qc = ro, batch_qc = batch))
+vis.gam(model2r, view = c("ro_qc", "batch_qc"),
+        theta = 50, n.grid = 50, lwd = 0.4)
+predict_gam2r = predict(model2r, newdata=data.frame(ro_qc = ro, batch_qc = batch))
+
+# with simple s term
+model1 <- gam(log2(f_int) ~ s(ro_qc))
+predict_gam1 = predict(model1, newdata=data.frame(ro_qc = ro, batch_qc = batch))
+
+# with interaction s term
+model2 <- gam(log2(f_int) ~ s(ro_qc, batch_qc))
+predict_gam2 = predict(model2, newdata=data.frame(ro_qc = ro, batch_qc = batch))
+vis.gam(model2, view = c("ro_qc", "batch_qc"),
+        theta = 50, n.grid = 50, lwd = 0.4)
+
+#...........................basic plot...........................
+plot(y=log2(f_int), x = ro_qc, xlab = "Order", ylab = "log2(TIC)", main='GAM Modeling')
+lines(predict_gam1, col='red', lwd = 2)
+lines(predict_gam2, col='blue', lwd = 2)
+lines(predict_gam1r, col='darkgreen', lwd = 2)
+lines(predict_gam2r, col='orange', lwd = 2)
+legend('topright', legend=c('s(ro_qc)', 's(ro_qc, batch_qc)', 's(ro_qc, by = batch_qc)', 's(ro_qc)+s(batch_qc)'),
+       col=c('red', 'blue', "darkgreen", "orange"), pch=19)
+
+#.............................ggplot.............................
+init_data <- as.data.frame(cbind(Abundance = log2(f_int), Order = ro_qc, Batch = batch_qc))
+data <- as.data.frame(cbind(Order = ro, 
+                            's(order)' = predict_gam1,
+                            's(order, batch)' = predict_gam2,
+                            's(order, by=batch)' = predict_gam1r,
+                            's(order)+s(batch)' = predict_gam2r))
+data <- melt(data, id = "Order")
+
+ggplot(init_data, aes(y = Abundance, x = Order)) + 
+  geom_point(size=3.5, color="black", fill= "white", shape=21, stroke=1) +
+  theme_classic(base_size = 15) + theme(legend.position = c(0.2, 0.25)) + labs(fill = "Batch")+labs(colour = "Model:") +
+  geom_line(data = data, aes(x = Order, y = value, colour = variable), alpha = 1, 
+            linewidth = 1.2)+scale_color_d3() 
+
+ggplot(init_data, aes(y = Abundance, x = Order)) + 
+  geom_point(size=3.5, color="black", fill= "grey90", shape=21, stroke=1) + 
+  theme_linedraw(base_size = 15) + theme(legend.position = "top", legend.title=element_blank(),
+                                         legend.key.height  = unit(0.02, 'cm'), legend.text = element_text(size=15)) + 
+  labs(fill = "Batch")+labs(colour = "Model:") +
+  geom_line(data = data, aes(x = Order, y = value, color = variable), alpha = 1, linewidth = 1.5)+scale_color_jco() + 
+  guides(color = guide_legend(keyheight = unit(0.1, 'cm')))
+
+ggplot(init_data, aes(y = Abundance, x = Order)) + 
+  geom_point(size=3.5, color="black", aes(fill= as.factor(Batch)), alpha=0.5, shape=21, stroke=0.5) + scale_fill_viridis_d(guide = 'none')+
+  theme_bw(base_size = 15) + theme(legend.position = "none") + labs(fill = "Batch")+labs(colour = "Model:") +
+  geom_line(data = data, aes(x = Order, y = value, colour = variable), alpha = 1, linewidth = 1.5)+scale_color_aaas()+
+  facet_grid(~variable)
+
 #................................................................
